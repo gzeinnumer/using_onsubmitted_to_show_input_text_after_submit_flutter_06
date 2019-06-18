99913d3e65bf4535d15c457b3834d1ea9924a8e7
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2019-06-19 00:04:11 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# using_onsubmitted_to_show_input_text_after_submit_flutter_06
---
+++ android/app/src/main/java/com/example/using_onsubmitted_to_show_input_text_after_submit_flutter_06/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.using_onsubmitted_to_show_input_text_after_submit_flutter_06;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
